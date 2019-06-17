3e75fb5c4bbdb4b90f17d1ca0638b72dbb42061c
yuvith <getyuvith@Github.com>
2019-06-17 17:10:54 +0000

    new shell script

---
+++ newshell.sh
@@ -0,0 +1,7 @@
+echo -e "{
+ 'Hostname':'`curl http://169.254.169.254/latest/meta-data/local-hostname --silent`', \
+\n 'AMI-ID':'`curl http://169.254.169.254/latest/meta-data/ami-id --silent`', \
+\n 'Kernel-Version':'`rpm -q kernel`', \
+\n 'SSM-version': '`rpm -qa |grep -i amazon-ssm-agent`'\
+\n 'Instance Type':'`curl http://169.254.169.254/latest/meta-data/instance-type --silent`'
+ }"
